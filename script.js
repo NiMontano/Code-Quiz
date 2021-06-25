133df8c949cf3a79701258e5a9457b0b791d110b
Denise Montano <benita5028@gmail.com>
2021-06-25 02:30:23 -0400

    added Q&AS and corresponding variables

--- script.js
+++ script.js
@@ -4,7 +4,32 @@ var startButton = document.querySelector(".start-btn")
 
 var timeLeft = 80;
 
-function setTime() {
+var quizQuestions = { 
+    question:   'Question 1',
+    answers: [
+        'Answer 1a',
+        'Answer 1b',
+        'Answer 1c',
+        'Answer 1d'
+    ],
+
+    question: 'Question 2',
+    answers: [
+        'Answer 2a',
+        'Answer 2b',
+        'Answer 2c',
+        'Answer 2d'
+    ]
+};
+
+var quizSection = document.createElement("ul");
+var listQnas = document.createElement("li");
+var listQuestions = document.createTextNode(quizQuestions.question);
+var listAnswers = document.createTextNode(quizQuestions.answers[i])
+
+
+
+function startQuiz() {
     var timerInterval = setInterval(function() {
       timeLeft--;
       timeEl.textContent = "Time : " + timeLeft ;
@@ -15,12 +40,23 @@ function setTime() {
       }
   
     }, 1000);
+
+    
+
+    //hide button
+    //grab section to show questions
+
+    //function to show quiz questions
+    //function showQuestions() {
+
+    //}
+
     console.log(timeLeft);
     console.log(timeEl);
   }
-  
+
 startButton.addEventListener("click", function(){
-    setTime();
+    startQuiz();
 })
 
 
